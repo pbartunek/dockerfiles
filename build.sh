7a77605d2228ac20b9807cfbc03ff3b9268491c7
Pawel <pawel.bartunek@gmail.com>
2018-04-07 11:04:14 +0200

    build script update

--- build.sh
+++ build.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-for dir in ./*/
-do
-  image=$(basename $dir);
-  echo "Building image: ${image}";
+function build() {
+  local image=$1
+  local dir=$2
+
   docker build --rm --force-rm -t $image $dir;
-done
+}
+
+if [ -z "$1" ]; then
+  echo "Building all images"
+  for dir in ./*/
+  do
+    image=$(basename $dir);
+    echo "Building image: ${image}";
+    build $image $dir
+  done
+else
+  for image in "$@"
+  do
+    if [[ -d "./${image}" ]]; then
+      echo "Building ${image}"
+      build $image "./${image}/"
+    else
+      echo "Could not find Dockerfile for image: ${image}, skipping.";
+    fi
+  done
+fi
