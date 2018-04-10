140385f99c3ca5eb657fac049c3dcdaa26148880
Pawel <pawel.bartunek@gmail.com>
2018-04-10 16:13:11 +0200

    webtest update

--- webtest/start.sh
+++ webtest/start.sh
@@ -2,6 +2,12 @@
 
 chromium --no-sandbox \
 	--proxy-server=http://127.0.0.1:8080 &> /dev/null &
-BurpSuitePro \
-  --config-file=/data/project.json \
-  --project-file=/data/project/project.burp &> /dev/null &
+
+if [ -f /data/project/project.burp ]; then
+  BurpSuitePro \
+    --project-file=/data/project/project.burp &> /dev/null &
+else
+  BurpSuitePro \
+    --config-file=/data/project/project.json \
+    --project-file=/data/project/project.burp &> /dev/null &
+fi
